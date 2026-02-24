ee20ee2b7bc6f30c7b06037086eec1fe36ec15db
HelloDearSir <jonbarton@hotmail.co.uk>
2026-02-24 23:57:07 +0000

    grabbing api from https://xboxoriginalapi-production.up.railway.app/games/"

---
+++ app/Http/Controllers/XboxGamesController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+class XboxGames extends Controller
+{
+        /* 
+        * @return response()
+        */
+    public function index(Request $resquest) {
+        $response = Http::get("https://xboxoriginalapi-production.up.railway.app/games/");
+        $posts = $response->json();
+        dd($posts);
+    }
+    }
