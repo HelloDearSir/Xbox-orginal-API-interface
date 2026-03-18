c3da902e868c8854918a93c037cf18b3d96ff31e
HelloDearSir <jonbarton@hotmail.co.uk>
2026-03-18 22:39:23 +0000

    Update XboxGamesController.php

--- app/Http/Controllers/XboxGamesController.php
+++ app/Http/Controllers/XboxGamesController.php
@@ -10,6 +10,7 @@ class XboxGames extends Controller
         * @return response()
         */
     public function index(Request $resquest) {
+        // response to the URL
         $response = Http::get("https://xboxoriginalapi-production.up.railway.app/games/");
         $posts = $response->json();
         dd($posts);
